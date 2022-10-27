a5169d78a9276f61815281ddf3439bdcea4fc099
Pauli Latva-Kokko <pauli.latva-kokko@tuni.fi>
2022-10-27 11:16:22 +0300

    Add animation for frontpage images

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,25 @@
 "use strict";
 
-var counter = 1;
+// Start animations on load.
+window.addEventListener("DOMContentLoaded", animate(), false);
 
-function setTimer() {
-    setInterval(changeImage, 5000);
+function animationListeners(imgs, fadeAnimation) {
+  /*
+  Add event listeners for images for animationend event. 
+  */
+  for(var i=0; i < imgs.length; i++) {
+    imgs[i].addEventListener("animationend", fadeAnimation, false);
+  }
 }
 
-function changeImage() {
-    if (counter > 3) {
-        counter = 1;
-    }
-    document.getElementById("background").src = "media/alkrakennus" + String(counter) + ".jpg";
-    counter += 1;
+
+
+function animate() {
+  /*
+  Change the first picture to the last in the div element. 
+  */
+    var stage = document.getElementById("slideshow");
+    function firstToLast() { stage.appendChild(imgs[0]); };
+    var imgs = stage.getElementsByTagName("img");
+    animationListeners(imgs, firstToLast);
 }
